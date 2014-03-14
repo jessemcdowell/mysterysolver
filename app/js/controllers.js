0bfacce462f460a288f12421a229dc89638d67a7
Jesse <jessemcd1@gmail.com>
2014-03-14 00:28:21 -0700

    rename myApp to mysterysolver

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,7 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
+angular.module('mysterysolver.controllers', []).
   controller('MyCtrl1', [function() {
 
   }])
--- test/lib/angular/angular-mocks.js
+++ test/lib/angular/angular-mocks.js
@@ -1788,8 +1788,8 @@ angular.module('ngMockE2E', ['ng']).config(['$provide', function($provide) {
  * on the `ngMockE2E` and your application modules and defines the fake backend:
  *
  * ```js
- *   myAppDev = angular.module('myAppDev', ['myApp', 'ngMockE2E']);
- *   myAppDev.run(function($httpBackend) {
+ *   mysterysolverDev = angular.module('mysterysolverDev', ['mysterysolver', 'ngMockE2E']);
+ *   mysterysolverDev.run(function($httpBackend) {
  *     phones = [{name: 'phone1'}, {name: 'phone2'}];
  *
  *     // returns the current list of phones
@@ -2078,7 +2078,7 @@ if(window.jasmine || window.mocha) {
    *       .value('version', 'v1.0.1');
    *
    *
-   *   describe('MyApp', function() {
+   *   describe('mysterysolver', function() {
    *
    *     // You need to load modules that you want to test,
    *     // it loads only the "ng" module by default.
